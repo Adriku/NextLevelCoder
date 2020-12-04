eba4a71c78af82fcd639fa89571a17ae2f61f253
Adriku <74843095+Adriku@users.noreply.github.com>
2020-12-04 14:47:28 -0400

    CLASE 8
    CLASE 8

--- components/ball.py
+++ components/ball.py
@@ -3,22 +3,27 @@ import random
 
 from utils.constants import (
     BLUE,
-    SCREEN_HEIGHT,
     SCREEN_WIDTH,
+    SCREEN_HEIGHT,
+    IMG_DIR,
+    BLACK,
 )
-allowed_speed = list(range(3,7))
+from os import path
+allowed_speed = list(range(3, 7))
+
+
 class Ball(pygame.sprite.Sprite):
     def __init__(self, size):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((38,40))
-        self.image.fill(BLUE)
+        self.image = pygame.image.load(path.join(IMG_DIR, "blue.png")).convert()
+        self.image = pygame.transform.scale(self.image, (100// size, 100// size))
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.x = random.randrange(SCREEN_WIDTH - self.rect.width)
         self.rect.y = random.randrange(-100, -40)
         self.speedy = random.choice(allowed_speed)
         self.speedx = random.choice(allowed_speed)
         self.size = size
-
 
     def update(self):
         self.rect.x = self.rect.x + self.speedx
@@ -29,14 +34,9 @@ class Ball(pygame.sprite.Sprite):
         if self.rect.left < 0:
             self.rect.left = 0
             self.speedx = random.choice(allowed_speed)
-
         if self.rect.bottom > SCREEN_HEIGHT:
-            self.rect.bottom > SCREEN_HEIGHT
+            self.rect.bottom = SCREEN_HEIGHT
             self.speedy = random.choice(allowed_speed) * -1
-
         if self.rect.top < 0:
             self.rect.top = 0
             self.speedy = random.choice(allowed_speed)
-
-
-
--- utils/constants.py
+++ utils/constants.py
@@ -1,3 +1,5 @@
+from os import path
+
 #SCREEN
 SCREEN_HEIGHT = 480
 SCREEN_WIDTH = 800
@@ -10,3 +12,7 @@ RED = (255, 0, 0)
 GREEN = (0, 255,0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
+
+IMG_DIR = path.join(path.dirname(__file__), "..", "img")
+
+FPS = (60)
